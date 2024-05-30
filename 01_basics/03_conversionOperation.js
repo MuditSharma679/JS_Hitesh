7d629a83a5158de9826b465110b1e11d1f727ffb
Mudit Sharma <mudit679@gmail.com>
2024-05-30 19:05:51 +0530

    operationsDiscussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -2,7 +2,6 @@ let score = "33";
 let score_2 = "33ab"; // This is not a pure number
 
 // console.log(typeof score);
-// console.log(typeof score);
 
 let valueInNumber = Number(score);
 let valueInNumber_2 = Number(score_2);
@@ -18,7 +17,7 @@ let valueInNumber_2 = Number(score_2);
 let isLoggedIn = 1;
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => false
@@ -27,5 +26,52 @@ console.log(booleanIsLoggedIn);
 let anyNumber = 33;
 let stringNumber = String(anyNumber);
 
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************** Operations ******************************/
+
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
+
+// console.log(2 + 2);
+// console.log(2 - 2);
+// console.log(2 * 2);
+// console.log(2 ** 3);
+// console.log(2 / 3);
+// console.log(2 % 3);
+
+let str1 = "hello";
+let str2 = " Mudit";
+let str3 = str1 + str2;
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+let x = 3;
+const y = x++; // Postfix
+
+// console.log(`x:${x}, y:${y}`);
+
+let a = 3;
+const b = ++a; //Prefix
+
+// console.log(`a:${a}, b:${b}`);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);

da8e01da2f41b6b3ec42ce21e14db835bafaf367
Mudit Sharma <mudit679@gmail.com>
2024-05-30 07:38:29 +0530

    03_conversionOperation.js

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33";
+let score_2 = "33ab"; // This is not a pure number
+
+// console.log(typeof score);
+// console.log(typeof score);
+
+let valueInNumber = Number(score);
+let valueInNumber_2 = Number(score_2);
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(valueInNumber_2);
+
+// "33" => 33
+// "33ab" => NaN
+// true => 1 ; false => 0
+
+let isLoggedIn = 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "mudit" => true
+
+let anyNumber = 33;
+let stringNumber = String(anyNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);

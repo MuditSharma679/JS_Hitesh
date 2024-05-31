3e966ff81653bd02d29582f341d9011a046879a7
Mudit Sharma <mudit679@gmail.com>
2024-05-31 07:52:08 +0530

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,41 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+const score = 100;
+const scoreValue = 100.3;
+
+const isLoggedIn = false;
+const outsideTemp = null;
+let userEmail;
+
+const id = Symbol("123");
+const anotherId = Symbol("123");
+
+// console.log(id === anotherId);
+
+// const bigNumber = 346464967739;
+
+// null is of type = object
+
+// Reference (Non-primitive)
+
+// Array, Objects, Functions
+
+const heros = ["Tata", "BharatBenz", "Hyundai"];
+let myObj = {
+  name: "mudit",
+  age: "25",
+};
+
+const myFunction = function () {
+  console.log("Hello World");
+};
+
+// console.log(typeof isLoggedIn);
+
+let myYoutubename = "sansCariMudit";
+anothername = "Lion";
+
+console.log(myYoutubename);
+console.log(anothername);

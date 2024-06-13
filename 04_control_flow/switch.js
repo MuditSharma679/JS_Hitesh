180408a9c27e22ae7db418548fe4284743f1bd6c
Mudit Sharma <mudit679@gmail.com>
2024-06-13 13:59:01 +0530

    Switch-case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,28 @@
+// *** Syntax for Switch-case
+// switch (key) {
+//   case value:
+//     break;
+
+//   default:
+//     break;
+// }
+
+const month = "Feb";
+
+switch (month) {
+  case "Jan":
+    console.log("January");
+    break;
+  case "Feb":
+    console.log("February");
+    break;
+  case "March":
+    console.log("March");
+    break;
+  case "April":
+    console.log("April");
+    break;
+  default:
+    console.log("Default case matched");
+    break;
+}

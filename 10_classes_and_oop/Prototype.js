77a46957b2c8bd2bb0e04303ce2643bc763c7a5f
Mudit Sharma <mudit679@gmail.com>
2024-08-15 19:50:03 +0530

    prototype

---
+++ 10_classes_and_oop/Prototype.js
@@ -0,0 +1,29 @@
+let myName = "hitesh    ";
+let myChannel = "sansCari  ";
+
+console.log(myName.trueLength);
+console.log(myChannel.trim().length);
+
+let myHeros = ["thor", "spiderman"];
+
+let heroPower = {
+  thor: "hammer",
+  spiderman: "sling",
+
+  getSpiderPower: function () {
+    console.log(`Spidy power is ${this.spiderman}`);
+  },
+};
+
+Object.prototype.hitesh = function () {
+  console.log(`hitesh is present in all objects`);
+};
+
+Array.prototype.heyHitesh = function () {
+  console.log(`Hitesh says hello`);
+};
+
+// heroPower.hitesh()
+// myHeros.hitesh()
+// myHeros.heyHitesh()
+// heroPower.heyHitesh()
